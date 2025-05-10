72b5cf15f49475278c4ada4a994f7ec7687c1906
Muugii0101 <one.munkhzul@gmail.com>
2025-05-10 14:30:40 +0800

    fix: error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Tabs } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
